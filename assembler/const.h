04b15ab97343f7632b353bdb9cf01012f90e5169
iGalich <89650689+iGalich@users.noreply.github.com>
2022-03-09 18:18:40 +0200

    Fixed spelling

--- assembler/const.h
+++ assembler/const.h
@@ -1,3 +1,3 @@
 #define STARTING_ADDRESS 100
-#define LAST_ADRESS 8191 /* Final adress of our RAM */
+#define LAST_ADRESS 8191 /* Final address of our RAM */
 #define MAX_LENGTH 81 /* max amount of characters in a single line in the files, including '\n' character */
